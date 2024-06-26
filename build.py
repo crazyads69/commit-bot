21e0193a68f087e232b5d81e8003dc3f9b0db023
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-06-26 21:20:53 +0700

    build: install commit-bot executable globally
    This commit installs the commit-bot executable globally, making it accessible from any directory.
    
    The executable is copied to the /usr/local/bin directory and permissions are set to ensure proper execution.

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 # Use pyinstaller to create an executable file from commit-bot.py. Run the executable file to generate a commit message and commit the changes to the repository.
 
+from re import sub
 import subprocess
 
 print("Building commit-bot executable...")
@@ -8,3 +9,30 @@ subprocess.run(
     check=True,
 )
 print("Commit-bot executable built successfully.")
+
+# Copy the excutable to the current directory
+print("Copying commit-bot executable...")
+subprocess.run(
+    ["cp", "-r", "dist/commit-bot", "."],
+    check=True,
+)
+
+# Change the permissions of the executable
+print("Changing permissions of commit-bot executable...")
+subprocess.run(
+    ["chmod", "+x", "commit-bot"],
+    check=True,
+)
+
+# Copy the commit-bot executable to the /usr/local/bin directory
+print("Copying commit-bot executable to /usr/local/bin...")
+print("You may be prompted for your password.")
+subprocess.run(
+    ["sudo", "cp", "-r", "commit-bot", "/usr/local/bin/"],
+    check=True,
+)
+subprocess.run(
+    ["sudo", "chmod", "755", "/usr/local/bin/commit-bot"],
+    check=True,
+)
+print("Commit-bot executable has been successfully installed.")
